4b8dbad538018f53dbe54a15522ed4727768c789
Callie Riggins <callie.riggins@airbnb.com>
2021-10-03 08:54:21 -0700

    New logo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { Player } from '@lottiefiles/react-lottie-player';
+import { useEffect, useRef, useState} from 'react';
+
+import LoopingLogo from '../assets/logo_loop_animation.json';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [logoAnimationPlayer, setLogoAnimationPlayer] = useState();
+
+  useEffect(() => {
+    if (!logoAnimationPlayer) return;
+
+    setTimeout(() => {
+      logoAnimationPlayer.state.instance.play();
+    }, 300);    
+  }, [logoAnimationPlayer, logoAnimationPlayer?.state?.instance]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +25,16 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+        <Player
+          className={styles.logoAnimation}
+          ref={setLogoAnimationPlayer}
+          controls={true}
+          src="https://assets4.lottiefiles.com/private_files/lf30_zwsvsve1.json"
+          speed={1.5}
+        />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
